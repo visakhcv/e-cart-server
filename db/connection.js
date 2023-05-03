e65c842d709954e7082102aa2efa54d7a6f0c0e2
visakhcv <visakhvisa12345@gmail.com>
2023-05-04 04:02:41 +0530

    dynamic port

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,8 @@ const mongoose=require('mongoose')
 const db= process.env.database
 
 mongoose.connect(db,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
+    useUnifiedTopology:"true",
+    useNewUrlParser:"true"
 }).then(()=>{
     console.log('database connected');
 }).catch((err)=>{
